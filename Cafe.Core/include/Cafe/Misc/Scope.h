d9ee5b2bc58e5ff1debefaf3b2bba39da2d1322c
akemimadoka <jxhzq1996@126.com>
2019-06-20 17:12:23 +0800

    小更新

---
+++ Cafe.Core/include/Cafe/Misc/Scope.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "TypeTraits.h"
+
+namespace Cafe::Core::Misc
+{
+	struct AlwaysInvoke
+	{
+		template <typename T>
+		static constexpr bool ShouldInvoke(T const&) noexcept
+		{
+			return true;
+		}
+	};
+
+	struct InvokeOnError
+	{
+		template <typename T>
+		static bool ShouldInvoke(T const&) noexcept
+		{
+			return std::uncaught_exceptions();
+		}
+	};
+
+	template <typename Callable, typename InvokeCondition = AlwaysInvoke>
+	class Scope
+	{
+	public:
+		template <typename... Args>
+		explicit Scope(Args&&... args) noexcept(std::is_nothrow_constructible_v<Callable, Args&&...>)
+		    : m_Callable(std::forward<Args>(args)...)
+		{
+		}
+
+		~Scope() noexcept(std::is_nothrow_invocable_v<Callable>)
+		{
+			if (InvokeCondition::ShouldInvoke(m_Callable))
+			{
+				m_Callable();
+			}
+		}
+
+	private:
+#if __has_cpp_attribute(no_unique_address) >= 201803L
+		[[no_unique_address]]
+#endif
+		Callable m_Callable;
+	};
+
+	namespace Detail
+	{
+		template <typename InvokeCondition>
+		struct ScopeMakerType : Core::Misc::Identity<InvokeCondition>
+		{
+			constexpr ScopeMakerType() noexcept = default;
+		};
+
+		template <typename InvokeCondition>
+		constexpr ScopeMakerType<InvokeCondition> ScopeMaker{};
+
+		template <typename Callable, typename InvokeCondition>
+		constexpr Scope<Core::Misc::RemoveCvRef<Callable>, InvokeCondition>
+		operator*(ScopeMakerType<InvokeCondition>, Callable&& callable) noexcept(
+		    std::is_nothrow_constructible_v<Scope<Core::Misc::RemoveCvRef<Callable>, InvokeCondition>,
+		                                    Callable&&>)
+		{
+			return Scope<Core::Misc::RemoveCvRef<Callable>, InvokeCondition>{ std::forward<Callable>(
+				  callable) };
+		}
+	} // namespace Detail
+} // namespace Cafe::Core::Misc
+
+#define CAFE_SCOPE_EXIT ::Cafe::Core::Misc::Detail::ScopeMaker<::Cafe::Core::Misc::AlwaysInvoke> * [&]
+#define CAFE_SCOPE_FAIL ::Cafe::Core::Misc::Detail::ScopeMaker<::Cafe::Core::Misc::InvokeOnError> * [&]
