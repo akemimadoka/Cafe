eab7d409e2117624f61012842626530b61ab4911
akemimadoka <chino@hotococoa.moe>
2021-05-06 00:26:39 +0800

    修改工程结构

--- Cafe.Core/Test/TypeTraits.Test.cpp
+++
@@ -1,8 +0,0 @@
-#include <Cafe/Misc/TypeTraits.h>
-
-using namespace Cafe;
-using namespace Core;
-using namespace Misc;
-
-static_assert(std::is_same_v<FindFirst<BindFrontTrait<std::is_same, int>::template Result, bool, int, double>, int>);
-static_assert(std::is_same_v<FindFirstOr<BindFrontTrait<std::is_same, float>::template Result, void, bool, int, double>, void>);
